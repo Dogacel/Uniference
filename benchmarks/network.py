872f1e5a65c0eaf63c7218ff7633f58640cc08ae
Doğaç Eldenk <dogacel@gmail.com>
2025-10-12 19:54:44 -0500

    try to prevent network overload

--- benchmarks/network.py
+++ benchmarks/network.py
@@ -132,6 +132,8 @@ def run(num_latency_tests=100, num_bw_tests=10, mode="send_recv"):
                 elif mode == "all_gather":
                     dist.all_gather(to_gather, big)
             dist.barrier()
+            print("Sleeping for 0.1s to avoid overloading the network...")
+            time.sleep(0.1)
 
         if rank == 0:
             mean_bw = sum(bw_results) / len(bw_results)
@@ -154,6 +156,9 @@ def run(num_latency_tests=100, num_bw_tests=10, mode="send_recv"):
             print(
                 f"[rank0] Size={size_str} | Transfer time (s): min={min(time_results):.4f}, max={max(time_results):.4f}, mean={mean_time:.4f}, std={std_time:.4f}"
             )
+        
+        print(f"Sleeping for 1s to avoid overloading the network...")
+        time.sleep(1.0)
 
     train_model(
         rtt=np.array(latencies).mean(),
